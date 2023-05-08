f0fcb059b9c0fc86ed13607dfb516d8c31962ff8
IbrahimElshafey <ibrahim.ragab.net@gmail.com>
2023-05-08 17:02:40 +0300

    Working on UI

--- ResumableFunctions.AspNetService/Controllers/RfUiController.cs
+++ ResumableFunctions.AspNetService/Controllers/RfUiController.cs
@@ -19,7 +19,7 @@ namespace MVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult ServiceDetails()
         {
             return View();
         }
